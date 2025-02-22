a1290dba70a3b43eb74f5355a8375edc3296f670
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-02-22 23:36:34 +0530

    SetUp New Project E-CD

---
+++ E-commerce Dashbord/frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-4xl text-black font-bold'>hello</h1>
+    </div>
+  );
+}
+
+export default App;
