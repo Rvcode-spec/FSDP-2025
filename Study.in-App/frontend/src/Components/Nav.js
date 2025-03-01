c107e8a6084d1fa39379c3c2361647f9dd256335
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-03-02 00:06:11 +0530

    Nav Page

--- Study.in-App/frontend/src/Components/Nav.js
+++ Study.in-App/frontend/src/Components/Nav.js
@@ -1,21 +1,14 @@
 import React from 'react'
-// import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function Nav() {
   return (
     <>
       <div className="w-full h-16 bg-[rgb(155,33,231)]  text-white flex items-center">
-      <div className="
-  drop-shadow-[1px_1px_2px_black,0_0_1em_rgb(209,237,67),0_0_0.2em_rgb(255,247,0)]">
-  Study.In
-</div>
-
         <ul className="flex justify-evenly w-full gap-x-6">
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
-          <li>Blog</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
 
