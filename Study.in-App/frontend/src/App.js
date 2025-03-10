d42a68649244a6c6fb9fb5f8681f0f1895182d41
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-03-11 00:12:30 +0530

    Footer Finished

--- Study.in-App/frontend/src/App.js
+++ Study.in-App/frontend/src/App.js
@@ -16,8 +16,8 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<SignUp/>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
-      <Footer/>
     </div>
   );
 }
--- Study.in-App/frontend/src/components/Footer.jsx
+++ Study.in-App/frontend/src/components/Footer.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
 const Footer = () => {
   return (
     <div className='footer'>
@@ -7,9 +10,18 @@ const Footer = () => {
       </div>
           <div className='footer-left-box'>
               <h3>Company</h3>
-          </div>
+              <ul>
+                <li><Link to="/about">About us</Link></li>
+                <li><Link to ='/contact'>Contact Us</Link></li>
+              </ul><br/>
+                <h3>Connect With Us</h3>
+                <ul>
+                <li><a href="mailto:info@example.com">Email us</a></li>
+                <li><a href="https://wa.me/919876543210" target="_blank" rel="noopener noreferrer">Talk to A Counselor</a></li>
+              </ul>
+          </div><br/><br/>
         
-      
+        <h5>Study.inCorporation © 2025</h5>
     </div>
   )
 }
