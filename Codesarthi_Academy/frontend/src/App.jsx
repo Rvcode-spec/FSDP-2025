1a00814badc05a6cf54270bde740bbd3be57afd5
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-06-26 22:11:43 +0530

    update

--- Codesarthi_Academy/frontend/src/App.jsx
+++ Codesarthi_Academy/frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useEffect } from 'react';
 
-export default function App() {
+
+function App() {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/test`)
+      .then(res => res.text())
+      .then(data => {
+        console.log("Response from backend:", data);
+      })
+      .catch(err => {
+        console.error("Error fetching backend:", err);
+      });
+  }, []);
+
   return (
-    <div className='text-2xl text-center '>
+    <>
+      <div className='text-2xl text-center '>
       <h1>hello</h1>
       
     </div>
-  )
+    </>
+  );
 }
+
+export default App;
--- Codesarthi_Academy/not.t
+++ Codesarthi_Academy/not.t
@@ -14,3 +14,25 @@ try{
         console.log("MongoDB connection failed:", error.message);
         process.exit(1);
     }
+
+    
+function App() {
+  useEffect(() => {
+    fetch(`${import.meta.env.BACKEND_API_URL}/api/test`)
+      .then(res => res.text())
+      .then(data => {
+        console.log("Response from backend:", data);
+      })
+      .catch(err => {
+        console.error("Error fetching backend:", err);
+      });
+  }, []);
+
+export default function App() {
+  return (
+    <div className='text-2xl text-center '>
+      <h1>hello</h1>
+      
+    </div>
+  )
+}
