2053bb888f06829a0c8db7269fd6624afa6f1f8a
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-07-01 12:33:31 +0530

    Backend IN Academy

--- Codesarthi-Academy/backend/config/connectDB.js
+++ Codesarthi-Academy/backend/config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB = async () =>{
+const connection = async () =>{
     try{
          await mongoose.connect(process.env.MONGO_URI);
          console.log("Conncetion Successfull");
@@ -12,5 +12,5 @@ const connectDB = async () =>{
     
 };
 
-module.exports = connectDB;
+module.exports = connection;
 
